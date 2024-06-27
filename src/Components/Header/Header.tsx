7be5356536cfe4668630149043b13e4593eaab56
kiokoeric <kiokoerick040@gmail.com>
2024-06-27 13:24:19 +0300

    Creation of the Header component in the Educate Website

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdHome } from "react-icons/io";
+import Logo from "../../assets/Educate_Logo.jpg";
+
+const Header: React.FC = () => {
+return (
+    <div>
+        <Link to="/" className='flex gap-4 items-center justify-center text-black no-underline'>
+            <img src={Logo} alt="" width="50px" />
+            <h1 className='font-bold text-3xl'>Educate</h1>
+        </Link>
+        <Link to="/" className='text-black no-underline'>
+            <IoMdHome size="1.8rem" color='black' />
+        </Link>
+        <section className="flex items-center justify-center gap-2" >
+        <Link to="/Favourites" className='' >
+            <Button ButtonText='Favourites Recipes' ButtonStyle='bg-lightOrange px-3 py-1 rounded text-base text-white' />
+        </Link>
+        {
+            <Link to="/Registration">
+                <Button
+                    ButtonText='Sign Up'
+                    ButtonStyle='bg-black cursor-pointer text-center text-base text-white px-5 py-1 rounded'
+                />
+            </Link>
+            }
+            {
+            !Cookie.auth_token ?
+            (
+                <Link to="/Login">
+                    <Button
+                        ButtonText='Login'
+                        ButtonStyle='bg-black cursor-pointer text-center text-base text-white px-5 py-1 rounded'
+                    />
+                </Link>
+            ) : 
+            (
+                <Button
+                    ButtonText='Logout'
+                    ButtonStyle='bg-black cursor-pointer h-8 text-center text-base text-white px-3 py-1 rounded'
+                    onClick={Logout}
+                />
+            )
+        }
+            <Link to={`/Profile`}>
+                <FaUser size="2rem" className="bg-black text-white cursor-pointer px-1.5 py-1.5 rounded-full" />
+            </Link>
+            { UserID ? <h4 className="font-bold flex flex-col text-center"><span>Welcome</span>{Name}</h4> : null }
+        </section>
+    </div>
+)
+}
+
+export default Header
