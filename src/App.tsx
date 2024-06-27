27f126af4cb4e43864a2cf63c7b08c57ed39798d
kiokoeric <kiokoerick040@gmail.com>
2024-06-27 12:59:56 +0300

    Creation of the Educate Website

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
