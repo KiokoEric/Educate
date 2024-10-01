0ff45412371af00bac3f9707e70d019a9a787ac1
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-01 20:13:15 +0300

    Update of the Home Page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { MdQuiz } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import Quiz from '../../assets/Quiz.jpg';
 import { IoIosCreate } from "react-icons/io";
+import { BsFillPatchQuestionFill } from "react-icons/bs";
+import Figure from '../../Components/Common/Figure/Figure';
 
 const Home:React.FC = () => {
 return (
-    <div id='Home' className=' flex flex-col justify-center text-black'>
-        <div className='flex flex-col items-center justify-center gap-20'>
-            <section className='m-auto w-3/4'>
-                <h1 className='mb-10 text-6xl text-center'>Welcome to <span className='text-green-800'>E</span>ducate</h1>
-                <p className='text-xl text-center'>Educate is the ultimate quiz application designed to test your knowledge across various topics, from history to pop culture, science to sports, and also allowing you to create your own quiz.</p>
+    <div id='Home' className='flex flex-col items-center justify-start sm:flex-row sm:justify-center mt-5 text-black'>
+        <div className='hidden sm:block' >
+            <img src={Quiz} alt="" />
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+            <section className='m-auto mb-5 w-11/12'>
+                <h1 className='mb-10 text-6xl text-center'>Welcome to <span className='text-Green'>E</span>ducate</h1>
+                <p className='m-auto text-xl text-center w-10/12'>Educate is the ultimate quiz application designed to test your knowledge across various topics, from history to pop culture, science to sports, and also allowing you to create your own quiz.</p>
             </section>
-            <section className='flex gap-10'>
-                <Link to={'/Settings'} className='cursor-pointer'>
-                    <MdQuiz size="6.5rem" />
-                    <figcaption className='text-center'>
-                        <p>Start Quiz</p>
-                    </figcaption>
-                </Link>
-                <Link to={"/"} className='cursor-pointer'>
-                    <IoIosCreate size="6.5rem" />
-                    <figcaption className='text-center'>
-                        <p>Create Quiz</p>
-                    </figcaption>
-                </Link>
+            <section className='flex gap-3 sm:gap-10'>
+                <Figure 
+                    Navigation={'/Settings'}
+                    children={<MdQuiz size="6.5rem" className='HomeIcons' />}
+                    NavigateText='Start Quiz'
+                />
+                <Figure 
+                    Navigation={'/Create'}
+                    children={<IoIosCreate size="6.5rem" className='HomeIcons' />}
+                    NavigateText='Create Quiz'
+                />
+                <Figure 
+                    Navigation={'/Quizzes'}
+                    children={<BsFillPatchQuestionFill size="6.5rem" className='HomeIcons' />}
+                    NavigateText='My Quizzes'
+                />
             </section>
         </div>
     </div>
