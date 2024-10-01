3a6b7690530cab4aa8273369943285205fc43925
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-01 20:52:19 +0300

    Update of the Main.tsx File

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import App from './App.tsx';
 import ReactDOM from 'react-dom/client';
 import { SnackbarProvider } from 'notistack';
 import { BrowserRouter } from "react-router-dom";
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'; // fas: Font Awesome Solid
 
 // Add the imported icons to the library
 library.add(fas);
