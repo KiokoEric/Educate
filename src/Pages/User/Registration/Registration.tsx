9d148fadb8020d1de53622db3347bb6dc6baf5b9
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-01 20:37:01 +0300

    Update of the Registration Page

--- src/Pages/User/Registration/Registration.tsx
+++ src/Pages/User/Registration/Registration.tsx
@@ -1,15 +1,13 @@
 import * as z from 'zod';
-import Axios from "axios";
+import axios from "axios";
 import React from 'react';
 import { useSnackbar } from 'notistack';
-import { useCookies } from "react-cookie";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Button from '../../../Components/Common/Button/Button';
 import RegistrationImage from "../../../assets/Registration_HomePage.jpg";
 
 const Registration:React.FC = () => {
-
     interface FormValues {
         Name: string;
         Email: string;
@@ -22,56 +20,66 @@ const Registration:React.FC = () => {
         Password: z.string().min(1, { message: 'Password is required'})
     });
 
-    const [_,setCookie] = useCookies(["auth_token"]);
     const { enqueueSnackbar } = useSnackbar();
 
     const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
         resolver: zodResolver(RegistrationSchema)
     });
 
-    const onRegistration : SubmitHandler<FormValues> = async (data) => {
+    type UserData = z.infer<typeof RegistrationSchema>
+
+    const onRegistration : SubmitHandler<FormValues> = async (data: UserData) => {
         try {
-            const response = await Axios.post("https://localhost:4000/Users/Registration", data) 
-                setCookie("auth_token", response.data.Token)
-                window.localStorage.setItem("UserID", response.data.UserID)
-                enqueueSnackbar("Logged in successfully!", {variant: "success"}) 
-                setTimeout(() => {
-                    window.location.reload();
-                }, 1500);
-        } catch (error) { 
-            enqueueSnackbar("Login unsuccessful!" , {variant: "error"})
-            console.log(error)
+            await axios.post("http://localhost:4000/Users/Registration", data) 
+            .then(() => {
+                enqueueSnackbar("Registration Completed! Kindly Log in", { 
+                    variant: 'success',
+                    anchorOrigin: {
+                        vertical: 'bottom',
+                        horizontal: 'right', 
+                    },
+                })
+            })
+        }catch (error) { 
+            enqueueSnackbar("Registration Failed!" , { 
+                variant: 'error',
+                anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                },
+            })  
+            console.error(error)
         }
     }
 
 return (
-    <div className='grid grid-cols-2 gap-5 items-center justify-center mt-1'>
-        <figure>
+    <div className='grid grid-cols-1 gap-5 items-center justify-center m-auto mt-1 w-11/12 sm:grid-cols-2'>
+        <figure className='hidden sm:block'>
             <img src={RegistrationImage} alt="" />
         </figure>
-        <form method="post" onSubmit={handleSubmit(onRegistration)} encType="multipart/form-data" className='flex flex-col items-center justify-start gap-2'>
-            <div className='mb-10'>
+        <form method="post" onSubmit={handleSubmit(onRegistration)} encType="multipart/form-data" className='flex flex-col items-center justify-start gap-2 m-auto w-11/12'>
+            <div className='my-10'>
                 <h2 className='text-5xl'>Welcome to <span className='text-green-800' >E</span>ducate</h2>
-                <p className='mt-5 text-center'>Create your account.</p>
+                <p className='mt-5 text-xl text-center'>Create your account.</p>
             </div>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="">Name</label> 
-                <input placeholder="Enter Name..." {...register('Name', { required: 'Name is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <input placeholder="Enter Name..." {...register('Name', { required: 'Name is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 lg:w-96' required />
                 {errors.Name && <p className="text-center text-red-700">{errors.Name.message}</p>}
             </div>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="Email">Email</label> 
-                <input placeholder="Enter Email..." {...register('Email', { required: 'Email is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <input placeholder="Enter Email..." {...register('Email', { required: 'Email is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 lg:w-96' required />
                 {errors.Email && <p className="text-center text-red-700">{errors.Email.message}</p>}
             </div>
             <div className='flex flex-col gap-2'>
                 <label className='font-bold' htmlFor="Password">Password</label> 
-                <input placeholder="Enter Password..." {...register('Password', { required: 'Password is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-96' required />
+                <input placeholder="Enter Password..." {...register('Password', { required: 'Password is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-2 text-black w-80 lg:w-96' required />
                 {errors.Password && <p className="text-center text-red-700">{errors.Password.message}</p>}
             </div>
             <Button
                 ButtonText='Sign Up'
-                ButtonStyle='bg-black cursor-pointer mt-1 text-center text-white px-3 py-1 rounded w-40'
+                ButtonStyle='bg-black cursor-pointer mt-5 text-center text-white px-3 py-1 rounded w-40'
                 onClick={handleSubmit(onRegistration)}
             />
         </form>
