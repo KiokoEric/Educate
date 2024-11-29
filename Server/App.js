cdae3d8492b8799da7feef58046bc4bcb717cb25
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-11-29 13:26:30 +0300

    Update App.js

--- Server/App.js
+++ Server/App.js
@@ -10,7 +10,7 @@ dotenv.config();
 // MIDDLEWARE
 
 app.use(cors({
-    origin: ["https://educate-rouge.vercel.app/"],
+    origin: ["https://educate-rouge.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],    
     credentials: true
 }))
